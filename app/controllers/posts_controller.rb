0803d50e3b25f1bccaf4111ecdfd7205401d92a4
martinbicycle <martinbicycle7700@gmail.com>
2021-02-07 13:10:21 +0000

    [Add]googlemapapi導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,22 +5,29 @@ class PostsController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    tackleids = PostTackle.where(post_id: params[:id] ).pluck(:tackle_id)
+    @tackles = Tackle.where(id: tackleids).pluck(:name).join(',')
     @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
+    @tackles = current_user.tackles
   end
 
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
+    params[:post][:tackle].each do |tackle|
+      post_tackle = PostTackle.new(post_id: @post.id, tackle_id: tackle)
+      post_tackle.save
+    end
     redirect_to post_path(@post)
   end
 
   def edit
+    @tackles = current_user.tackles
     @post = Post.find(params[:id])
   end
 
@@ -31,14 +38,17 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @user = current_user
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to user_path(@post)
+    redirect_to user_path(@user)
   end
 
   private
     def post_params
-      params.require(:post).permit(:name, :body, :image, :size, :fish_time, :address, :latitude, :longitude, :lure)
+      params.require(:post).permit(
+        :name, :body, :image, :size, :fish_time,
+        :address, :latitude, :longitude, :lure)
     end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
 
   attachment :image
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
   belongs_to :user
   has_many :tackles, :through => :post_tackles

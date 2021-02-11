0cccad4aacb1568f8c8516256e3656caa49980ae
martinbicycle <martinbicycle7700@gmail.com>
2021-02-11 13:46:03 +0000

    [Add]レスポンシブ修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,18 +16,18 @@ class Post < ApplicationRecord
   end
 
   def save_tags(savepost_tags)
-      current_tags = self.tags.pluck(:name) unless self.tags.nil?
-      old_tags = current_tags - savepost_tags
-      new_tags = savepost_tags - current_tags
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    old_tags = current_tags - savepost_tags
+    new_tags = savepost_tags - current_tags
 
-      old_tags.each do |old_name|
-        self.tags.delete Tag.find_by(name: old_name)
-      end
+    old_tags.each do |old_name|
+      self.tags.delete Tag.find_by(name: old_name)
+    end
 
-      new_tags.each do |new_name|
-        post_tag = Tag.find_or_create_by(name: new_name)
-        self.tags << post_tag
-      end
+    new_tags.each do |new_name|
+      post_tag = Tag.find_or_create_by(name: new_name)
+      self.tags << post_tag
+    end
   end
 
 

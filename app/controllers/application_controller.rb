3d85ff6c706c18807e47a6a4019f7691b419d52f
martinbicycle <martinbicycle7700@gmail.com>
2021-02-16 12:59:00 +0000

    [Add]管理者権限設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,26 @@
 class ApplicationController < ActionController::Base
+  helper_method :user_following?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   add_flash_types :success, :info, :warning, :danger
 
+
+  def user_following?(user)
+    if user_signed_in? && current_user != user && current_user.following?(user)
+      true
+    else
+      false
+    end
+  end
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def authenticate_all_user!
+    unless admin_signed_in? || user_signed_in?
+      new_user_session_path
+    end
   end
 end

bdb298587584de99f07e10edd6738fbb626470dc
martinbicycle <martinbicycle7700@gmail.com>
2021-02-17 13:38:39 +0000

    [Add]タグの検索機能修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,10 +6,12 @@ class SearchController < ApplicationController
 		@method = params[:method]
 		if @model == 'post'
 			@records = Post.search_for(@content, @method)
-		elsif
+		elsif @model == 'column'
 			@records = Column.search_for(@content, @method)
-		elsif
-			@records = Tag.search_for(@content, @method)
+		elsif @model == 'tag'
+			tag_ids = Tag.search_for(@content, @method).pluck(:id)
+			post_ids = PostTag.where(tag_id: tag_ids).pluck(:post_id)
+			@records = Post.where(id: post_ids)
 		end
 	end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,9 +2,10 @@ class Tag < ApplicationRecord
   has_many :post_tags, dependent: :destroy
   has_many :posts, :through => :post_tags
   validates :name, uniqueness: true
-end
 
 
   def self.search_for(content, method)
     Tag.where('name LIKE ?', '%'+content+'%')
   end
+
+end

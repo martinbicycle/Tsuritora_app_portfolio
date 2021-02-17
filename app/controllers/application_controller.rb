ddbf266aeed8652abfc43f9ae1449832b722ac2a
martinbicycle <martinbicycle7700@gmail.com>
2021-02-17 05:47:31 +0000

    [Add]レイアウト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,7 @@
 class ApplicationController < ActionController::Base
-  helper_method :user_following?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   add_flash_types :success, :info, :warning, :danger
-
-
-  def user_following?(user)
-    if user_signed_in? && current_user != user && current_user.following?(user)
-      true
-    else
-      false
-    end
-  end
 
   private
   def configure_permitted_parameters

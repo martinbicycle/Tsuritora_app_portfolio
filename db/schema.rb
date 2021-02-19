5b9e9827dfceb2d4442c941373dead275a8de993
martinbicycle <martinbicycle7700@gmail.com>
2021-02-19 05:19:23 +0000

    [Add]スキーマ修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_19_041645) do
+ActiveRecord::Schema.define(version: 2021_02_19_051640) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -53,17 +53,6 @@ ActiveRecord::Schema.define(version: 2021_02_19_041645) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "notifications", force: :cascade do |t|
-    t.integer "visiter_id"
-    t.integer "visited_id"
-    t.integer "item_id"
-    t.integer "comment_id"
-    t.string "action"
-    t.boolean "checked", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

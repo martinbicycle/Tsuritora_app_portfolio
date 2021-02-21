fa2146b853b5606250e8508bdf6b264302fbff7d
martinbicycle <martinbicycle7700@gmail.com>
2021-02-21 14:30:05 +0000

    [Add]トップページ修正、ターボリンクス解除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+
 //= require_tree .
 //= require underscore
 //= require gmaps/google
@@ -23,13 +23,13 @@
 $(document).ready(function () {
   $("#theTarget").skippr({
     // スライドショーの変化 ("fade" or "slide")
-    transition : 'fade',
+    transition : 'slide',
     // 変化に係る時間(ミリ秒)
     speed : 500,
     // easingの種類
     easing : 'easeOutQuart',
     // ナビゲーションの形("block" or "bubble")
-    navType : 'bubble',
+    navType : 'block',
     // 子要素の種類('div' or 'img')
     childrenElementType : 'div',
     // ナビゲーション矢印の表示(trueで表示)
@@ -37,9 +37,9 @@ $(document).ready(function () {
     // スライドショーの自動再生(falseで自動再生なし)
     autoPlay : true,
     // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 2000,
+    autoPlayDuration : 2500,
     // キーボードの矢印キーによるスライド送りの設定(trueで有効)
-    keyboardOnAlways : true,
+    keyboardOnAlways : false,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
     hidePrevious : false
   });
